0226f31c826a7def79dcf7929416a8ac736d049c
gen <genmanabu@gmail.com>
2025-06-11 05:29:47 +0900

    firebase認証とDB構成

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // ← ページやコンポーネントのパス
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "hsl(var(--background) / <alpha-value>)",
+        foreground: "hsl(var(--foreground) / <alpha-value>)",
+      },
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", "sans-serif"],
+        mono: ["var(--font-geist-mono)", "monospace"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
